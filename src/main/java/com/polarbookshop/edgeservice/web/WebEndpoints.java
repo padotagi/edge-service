3a64f38e23348ecc52c7952efdb6e2a01b400256
Seine <>
2024-09-15 02:49:45 +0900

    240914 added tilt, manifest, commit-stage and application test

--- src/main/java/com/polarbookshop/edgeservice/web/WebEndpoints.java
+++ src/main/java/com/polarbookshop/edgeservice/web/WebEndpoints.java
@@ -1,14 +1,14 @@
 package com.polarbookshop.edgeservice.web;
 
-import org.springframework.web.servlet.function.RouterFunction;
-import org.springframework.web.servlet.function.RouterFunctions;
-import org.springframework.web.servlet.function.ServerResponse;
 import reactor.core.publisher.Mono;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
 @Configuration
 public class WebEndpoints {
@@ -17,7 +17,7 @@ public class WebEndpoints {
     public RouterFunction<ServerResponse> routerFunction() {
         return RouterFunctions.route()
                 .GET("/catalog-fallback", request ->
-                        ServerResponse.ok().body(Mono.just("")))
+                        ServerResponse.ok().body(Mono.just(""), String.class))
                 .POST("/catalog-fallback", request ->
                         ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE).build())
                 .build();
